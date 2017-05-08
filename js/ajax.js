e64f10f958e8f5feb20b99d4316bb40ad6a24a3e
zoohan955 <ichekrigin@mail.ru>
2017-05-08 23:03:57 +1200

    refactoring

--- js/ajax.js
+++ js/ajax.js
@@ -1,31 +1,3 @@
-
-
-
-var inSearch = document.querySelector('.SearchInput');
-var NameType = document.querySelector("#startMenu-nameButton",".button");
-
-
-var button = document.querySelector('.load');
-var RTreeButton = document.querySelector('#RTreeMenuButton' , '.element');
-var GLandMenuButton = document.querySelector('#GlandMenuButton','#mainMenu');
-
-var  button1 = document.querySelector('.export');
-
-
-
-//GLAND
-var script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = 'module/library/GLand/GLand.js';    
-//
-
-//RTree
-var script2 = document.createElement('script');
-script2.type='text/javascript';
-script2.src = 'module/library/RTree/RTree.js';  
-//
-
-
 //objets
 function  User  (name,age ){
     this.name=name;
@@ -38,50 +10,8 @@ function  User  (name,age ){
 user1 = new User();
 
 //console.log(user1.name+" "+ user1.dificulty);
-
-RTreeButton.addEventListener('click', function(){
-//---------------------------------------------СОБЫТИЯ 
-//document.getElementsByTagName('head')[0].appendChild(script);
 /*
-fetch('module/menu/RTree/RTree.html')
-.then(function(data){
-    hd.append(RtreeS);
-  // $('#main_menu-layers').append(data);
-  // $("head").append(RtreeS);
-  
-    
-}, function(data){
-    console.log('error');
 
-
-//module/menu/RTree/RTree.html
-//$.get('module/menu/RTree/RTree.html')
-
-    });*/
-    
-    $.get('module/menu/RTree/RTree.html',function(data){
-    document.getElementsByTagName('head')[0].appendChild(script2);
-    $('#main_menu-layers').append(data);
-});
-
-
-});
-
-
-GLandMenuButton.addEventListener('click',function(){
-
-$.get('module/menu/GLand/GLand.html', function(data){
-$('#main_menu-layers').append(data);
-console.log("htm loaded");
-}).done(()=>{
-document.getElementsByTagName('head')[0].appendChild(script);
-console.log("htm loaded js loading");
-})
-});
-
-
-
-//GLandMenuButton.addEventListener('')
 
 //----------------------------------------------/СОБЫТИЯ
 
@@ -99,26 +29,5 @@ fetch('data.json')
 
 
 });
+*/
 
-button1.addEventListener('click', (e)=>{
-e.preventDefault();
-
-
-
-
-$.getJSON('data.json', function(data){
-    if (user1.dificulty()===1){
-     console.log(data.categories.category1);
-}
-   // console.log(data.categories.category1);
-
-});
-});
-
-/*
-
-for (var i = 0; i < Object.keys(JSON).length - 1; i++) {
-  if (JSON[i].match(eval('{' + ('/' + search_string + '/gi') + '}')) != null){
-    console.log(JSON);
-  }
-}*/
