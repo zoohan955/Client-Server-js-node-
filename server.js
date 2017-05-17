c5a07893385107eb101007f46a6219da0a75aef8
zoohan955 <ichekrigin@mail.ru>
2017-05-17 20:53:29 +1200

    Запрпос поиска и генерации тепер обрабатывает json файл. произведен рефакторинг кода сервера

--- server.js
+++ server.js
@@ -5,124 +5,61 @@ var static = require('node-static');
 var file = new static.Server('.');
 var fs = require('fs');
 
-//var server = http.createServer().listen(9000);
-
 function request(req, res) {
 
-  // если URL запроса /vote, то...
   if (req.url == '/vote') {
     // через 1.5 секунды ответить сообщением
-    setTimeout(function() {
+    setTimeout(function () {
       res.end('Ваш голос принят: ' + new Date());
     }, 1500);
-  } else {
-    // иначе считаем это запросом к обычному файлу и выводим его
-    file.serve(req, res); // (если он есть)
   }
+  else if (req.url == '/1.html') {
+    //text1 = fs.readFileSync("1.html");
+    text1 = fs.readFile('1.html', 'UTF-8', function (error, data) {
+      if (error) { return console.log(error) }
+      else { res.end(data); }
+    });
 
-  if(req.url=='/1.html'){
-      //text1 = fs.readFileSync("1.html");
-      text1 = fs.readFile('1.html','UTF-8',function(error, data){
-        if(error){return console.log(error)}
-        else {res.end(data);}
+  }
+  else if (req.url == '/2') {
+    req.on("data", (data) => {
+      res.end(menuGenerator(Finder(data), data, 'searchingResults'));
+    })
+    console.log('событие для поста');
+  }
+  else if (req.url == '/3') {
+    req.on("data", (data) => {
+      fs.readFile('data.json', 'UTF-8', function (error, content) {
+        res.end(JSON.stringify(menuGenerator(Finder(data, JSON.parse(content)), data, 'searchingResults')));
       });
-
-  } else {
-    // иначе считаем это запросом к обычному файлу и выводим его
-    file.serve(req, res); // (если он есть)
+    });
   }
-
-
-/*
- if(req.url=='/2.html'){
-      //text1 = fs.readFileSync("1.html");
-      text1 = fs.readFile('2.html','UTF-8',function(error, data){
-        if(error){return console.log(error)}
-        else {res.end(data);}
-      });
-
-  } else {
-    // иначе считаем это запросом к обычному файлу и выводим его
-    file.serve(req, res); // (если он есть)
-  }
-*/
-
-if(req.url=='/2')
-{
- req.on("data",(data)=>{
-   res.end(menuGenerator(Finder(data),data,'searchingResults'));
- })
-  console.log('событие для поста');
+  else file.serve(req, res);
+  console.log(`${req.method} ${req.url}`);
 }
- console.log(`${req.method} ${req.url}`);
+//----------------FUNCTIONS-------------------------
+function Finder(value, data) {
+  return data.exponats.filter(e => e.labels.some(l => l.match(value)));
 }
-
-function Finder(value){
- return data.exponats.filter(e=>e.labels.some(l=>l.match(value)));
-}
-
-function menuGenerator(array,title, idMenu){
-   var res='';
-   var menu=`<div class="menu display-none" id="`+idMenu+`">"
+function menuGenerator(array, title, idMenu) {
+  var res = '';
+  var menu = `<div class="menu display-none" id="` + idMenu + `">
             <div class="element">
-            <div class="element-title">`+title+`</div>
+            <div class="element-title">`+ title + `</div>
                     </div>`;
 
-  array.forEach((item,i,array)=>{
-  var element = ` <div class="element" id="`+i+`">"
+  array.forEach((item, i, array) => {
+    var element = ` <div class="element" id="` + i + `">
                         <div class="element-image">
-                            <img src="` + item.img +  `"/>
+                            <img src="` + item.img + `"/>
                         </div>
-                        <div class="element-title">`+item.name+`</div>
-                    </div->`;
-   res=res+element+`</div>`;
-  
+                        <div class="element-title">`+ item.name + `</div>
+                    </div>`;
+    res = res + element + `</div>`;
+
   });
-return menu+res;
+  return menu + res;
 }
-
-data = {
-        "exponats":[
-                                {       
-                                        "dir":"Rtree",
-                                        "img":"splash/RTreeMenu.png",
-                                        "name":"Рекурсивное дерево",
-                                        "html":"module/menu/RTree/RTree.html",
-                                        "scripts":"module/library/RTree/RTree.js",
-                                        "labels":["RTree", "Tree", "Дерево", "Рисовать"],
-                                        "discription":"описание для 1 категории",
-                                        "difficulty":1,
-                                        "quiz":{}
-                                },
-                                {                                        
-                                        "dir":"Rtree2",
-                                        "img":"splash/RTreeMenu.png",
-                                        "name":"Рекурсивное дерево",
-                                        "html":"module/menu/RTree/RTree.html",
-                                        "scripts":"module/library/RTree/RTree.js",
-                                        "labels":["RTree", "Tree", "Дерево", "Рекурсия", "Фрактал"],
-                                        "discription":"описание для 2 категории",
-                                        "difficulty":2,
-                                        "quiz":{}
-                                },
-                                {       
-                                        "dir":"GLand",
-                                        "img":"splash/Gland.png",
-                                        "discription_full":"описание для второй категории",
-                                        "name":"Генератор ландшафтов",
-                                        "html":"module/menu/GLand/GLand.html",
-                                        "scripts":"module/library/GLand/GLand.js",
-                                        "labels":["Gland", "Generator", "Ландшафт", "Рекурсия"],
-                                        "discription_simple":"описание для 1 категории",
-                                        "difficulty":2,
-                                        "quiz":{}
-                                }
-                ]
-                        
-}
-
-
-
 
 // ------ этот код запускает веб-сервер -------
 
@@ -131,12 +68,3 @@ if (!module.parent) {
 } else {
   exports.request = request;
 }
-
-/*
-server.addListener('request', function(req,res){
-     file.serve(req,res);
-
-console.log(`${req.method} ${req.url}`);
-});
-*/
-
