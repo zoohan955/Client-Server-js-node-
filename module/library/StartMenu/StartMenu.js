e971c159b9c7bd86ed81369c8e5def17266bd83d
zoohan955 <ichekrigin@mail.ru>
2017-06-17 03:40:00 +1200

    up

--- module/library/StartMenu/StartMenu.js
+++ module/library/StartMenu/StartMenu.js
@@ -15,12 +15,18 @@ startMenu.select("#startMenu-nameButton").on("click", function check() {
     var newElement = d3.select("#startMenu-year");
     currentElement_startMenu = replaceElement(currentElement_startMenu, newElement);
     tmpName = document.querySelector("#startMenu-nameInput").value;
+
 });
 startMenu.select("#startMenu-yearButton").on("click", () => {
     tmpAge = document.querySelector("#startMenu-yearInput").value;
     user1 = new User(tmpName, tmpAge);
+    checkAge(tmpAge);
     RemoveMenu($('#start_menu-layers'), $('#startMenuJS'));
     AddRemoveClass(d3.select("#demoVideo_menu-layers"), "display-none");
     currentVideo.play();
 });
 
+function checkAge(age){
+    if (Number.isInteger(age))  return (age>0)?age:"error" 
+    return "error";
+}
